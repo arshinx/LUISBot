a7d6d88042eff5d2314a1434c4183485e9e2fcce
Arshin Jain <arshin.jain@gmail.com>
2016-06-21 14:26:57 -0700

    Basic Bot - LUIS Configured

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 
-https://api.projectoxford.ai/luis/v1/application?id=c413b2ef-382c-45bd-8ff0-f76d60e2a821&subscription-key=d628a77deb694925bb8893c056dea7fb&q=
 
+
+var builder = require('botbuilder');
+
+// Create LUIS Dialog that points at our model and add it as the root '/' dialog for our Cortana Bot.
+var model = 'https://api.projectoxford.ai/luis/v1/application?id=c413b2ef-382c-45bd-8ff0-f76d60e2a821&subscription-key=d628a77deb694925bb8893c056dea7fb&q=';
+var dialog = new builder.LuisDialog(model);
+var cortanaBot = new builder.TextBot();
+cortanaBot.add('/', dialog);
+
+// Add intent handlers
+dialog.on('builtin.intent.alarm.set_alarm', builder.DialogAction.send('Creating Alarm'));
+dialog.on('builtin.intent.alarm.delete_alarm', builder.DialogAction.send('Deleting Alarm'));
+dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only create & delete alarms."));
+
+cortanaBot.listenStdin();
